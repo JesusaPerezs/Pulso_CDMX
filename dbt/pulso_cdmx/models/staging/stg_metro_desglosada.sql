a3a152e0c9b4727d6484432b5eea931c1f006a2a
Jesus Perez <jesusaperezs.05@gmail.comgit config --global user.email jesusaperezs.05@gmail.com>
2026-07-19 20:16:59 -0600

    Agrega staging desglosada: repara mojibake y agrega tipo_pago

---
+++ dbt/pulso_cdmx/models/staging/stg_metro_desglosada.sql
@@ -0,0 +1,31 @@
+with fuente as (
+    select * from {{ source('raw', 'metro_desglosada_raw') }}
+),
+
+limpio as (
+    select
+        fecha,
+        anio,
+        mes,
+        -- Normaliza mojibake: 'LÃ­nea X' -> 'Linea X'
+        replace(linea, 'LÃ­nea', 'Linea') as linea,
+        replace(replace(replace(replace(replace(replace(replace(
+        estacion,
+        'Miguel Ã ngel', 'Miguel Ángel'),
+        'Ã­', 'í'),
+        'Ã¡', 'á'),
+        'Ã³', 'ó'),
+        'Ã©', 'é'),
+        'Ã±', 'ñ'),
+        'Ãº', 'ú') as estacion,
+        tipo_pago,
+        afluencia
+    from fuente
+),
+marcado as (
+    select *,
+        'ok' as calidad_dato
+        from limpio
+)
+
+select * from marcado
